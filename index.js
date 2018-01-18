140ab494e57f0eddbea6523de0de3fb7200b2368
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-18 06:02:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ function getRepositories() {
   req.send();
 }
 
-req.addEventListener("load", showRepositories);
-  req.open("GET", 'https://api.github.com/users/octocat/repos')
-  req.send()
+function showRepositories() {
+  var repos = JSON.parse(this.responseText)
+  console.log(repos)
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + '- <a href="'+ r.html_url + '">URL</a> - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
+}
+
+var repos = JSON.parse(this.responseText)
+console.log(repos)
+const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
+document.getElementById("repositories").innerHTML = repoList
