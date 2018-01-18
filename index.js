0ce70b7a785705751b97895d0df8c2ee056b5e57
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-18 06:10:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ function getRepositories() {
 }
 
 function showRepositories() {
-  debugger
   var repos = JSON.parse(this.responseText)
   console.log(repos)
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + '- <a href="'+ r.html_url + '">URL</a> - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
