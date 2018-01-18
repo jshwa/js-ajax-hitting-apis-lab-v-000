ce52df2052114b47c18511a0becdc7695dbb5d35
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-18 06:09:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function getRepositories() {
   const username = document.getElementById("username").value;
-  console.log(username)
   const req = new XMLHttpRequest();
   req.addEventListener("load", showRepositories);
   req.open(`https://api.github.com/users/${username}/repos`);
@@ -8,6 +7,7 @@ function getRepositories() {
 }
 
 function showRepositories() {
+  debugger
   var repos = JSON.parse(this.responseText)
   console.log(repos)
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + '- <a href="'+ r.html_url + '">URL</a> - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
