1e04b6fa750a91f774983b43630984e9d5a6c933
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-18 06:24:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ function getCommits(el) {
 function displayCommits() {
   const commits = JSON.parse(this.responseText)
   console.log(this.responseText)
-  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.author.login + ' - ' + commit.commit.message + '</li>').join('')}</ul>`
   document.getElementById("details").innerHTML = commitsList
 }
