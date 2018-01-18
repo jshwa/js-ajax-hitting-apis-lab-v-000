c73fa69be03e06f80761be84993096ce0a229290
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-18 06:09:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function getRepositories() {
   const username = document.getElementById("username").value;
-  debugger
   console.log(username)
   const req = new XMLHttpRequest();
   req.addEventListener("load", showRepositories);
