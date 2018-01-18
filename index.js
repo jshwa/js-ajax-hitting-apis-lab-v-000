bdca1e380a8002a7c9fbc47ecd893ed90d954ccb
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-18 06:03:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,9 +10,5 @@ function showRepositories() {
   var repos = JSON.parse(this.responseText)
   console.log(repos)
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + '- <a href="'+ r.html_url + '">URL</a> - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
+  document.getElementById("repositories").innerHTML = repoList
 }
-
-var repos = JSON.parse(this.responseText)
-console.log(repos)
-const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
-document.getElementById("repositories").innerHTML = repoList
