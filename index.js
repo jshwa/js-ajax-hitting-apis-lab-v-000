5bf230e9d4664c5e31a31d9efb516033154cd9ca
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-18 06:19:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,13 @@ function showRepositories() {
 function getCommits(el) {
   const name = el.dataset.repo
   const req = new XMLHttpRequest()
-  req.addEventListener("load", showCommits)
+  req.addEventListener("load", displayCommits)
   req.open("GET", `${el.dataset.url}/commits`)
   req.send()
+}
+
+function displayCommits() {
+  const commits = JSON.parse(this.responseText)
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+  document.getElementById("commits").innerHTML = commitsList
 }
